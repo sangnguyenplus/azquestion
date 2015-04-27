9206d4e38087ca0cf8ce3dadfb66b8c7a318ca73
sangnguyenplus <sangnguyenplus@gmail.com>
2015-04-27 09:40:05 +0700

    Change config

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -45,14 +45,14 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/question/manage.html',
 		controller :'ListQuestionController',
       	title: 'Quản lý câu hỏi',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-question-edit',{
     	url: "/system/questions/edit/:id",
 		templateUrl : 'views/private/question/edit.html',
 		controller :'QuestionController',
       	title: 'Quản lý câu hỏi',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	/*===============ANSWERS================*/
 	.state('system-answer',{
@@ -60,14 +60,14 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/answer/manage.html',
 		controller :'AnswerController',
       	title: 'Quản lý câu trả lời',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-answer-detail',{
     	url: "/system/answers/detail/:id",
 		templateUrl : 'views/private/answer/detail.html',
 		controller :'AnswerController',
       	title: 'Quản lý câu trả lời',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
   	/*===============BADGES================*/
 	.state('badges',{
@@ -82,21 +82,21 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/badge/manage.html',
 		controller :'BadgeController',
       	title: 'Quản lý danh hiệu',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-badge-add',{
     	url: "/system/badges/add",
 		templateUrl : 'views/private/badge/add.html',
 		controller :'BadgeController',
       	title: 'Quản lý danh hiệu - Thêm mới',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-badge-edit',{
     	url: "/system/badges/edit/:id",
 		templateUrl : 'views/private/badge/edit.html',
 		controller :'BadgeController',
       	title: 'Quản lý danh hiệu - Sửa',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	/*===============USERS================*/
 	.state('users', {
@@ -111,7 +111,7 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/public/user/profile.html',
 		controller :'ProfileUserController',
       	title: 'Chi tiết tài khoản',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('forgot_password', {
 		url: "/thanh-vien/quen-mat-khau.html",
@@ -132,7 +132,7 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/public/user/edit.html',
 		controller :'EditProfile',
       	title: 'Chỉnh sửa thông tin',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('register', {
 		url: "/dang-ky.html",
@@ -160,14 +160,14 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/user/manage.html',
 		controller :'ListUserController',
       	title: 'Quản lý thành viên',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-user-permission', {
 		url: "/system/users/permission/:id",
 		templateUrl : 'views/private/user/permission.html',
 		controller :'EditProfile',
       	title: 'Quản lý thành viên',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	/*================SYSTEM================*/
 	.state('system', {
@@ -175,7 +175,7 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/system.html',
 		controller :'MainController',
       	title: 'Quản lý Hệ thống',
-      	access: { requiredLogin: false },
+      	access: { requiredLogin: true },
       	/*isadmin:{requireAdmin: true}*/
 	})
 	/*===============TAGS================*/
@@ -198,14 +198,14 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/tag/manage.html',
 		controller :'ListTagController',
       	title: 'Quản lý Tags',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-tag-edit',{
     	url: "/system/tags/edit/:id",
 		templateUrl : 'views/private/tag/edit.html',
 		controller :'TagDetail',
       	title: 'Quản lý Tags',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	/*================SYSTEM SETTING==============*/
 	.state('system-setting',{
@@ -213,35 +213,35 @@ angular.module('appRoutes',[]).config(['$stateProvider', '$urlRouterProvider','$
 		templateUrl : 'views/private/setting/index.html',
 		controller :'SystemController',
       	title: 'Cấu hình hệ thống',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-setting-about',{
 		url: "/system/setting/about",
 		templateUrl : 'views/private/setting/about.html',
 		controller :'SystemController',
       	title: 'Chỉnh sửa giới thiệu',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-setting-policy',{
 		url: "/system/setting/policy",
 		templateUrl : 'views/private/setting/policy.html',
 		controller :'SystemController',
       	title: 'Chỉnh sửa quy định & điều khoản',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-setting-help',{
 		url: "/system/setting/help",
 		templateUrl : 'views/private/setting/help.html',
 		controller :'SystemController',
       	title: 'Chỉnh sửa hướng dẫn',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	.state('system-report',{
 		url: "/system/report",
 		templateUrl : 'views/private/report/manage.html',
 		controller :'ReportController',
       	title: 'Quản lý vi phạm',
-      	access: { requiredLogin: false }
+      	access: { requiredLogin: true }
 	})
 	/*===============404 NOT FOUND================*/
 	.state('404', {
